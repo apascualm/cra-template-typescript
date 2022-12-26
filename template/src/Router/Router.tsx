780fdd636573d580998b348f4f763033db0aa158
Alejandro Pascual Montenegro <pascualmontenegro@gmail.com>
2022-12-26 13:55:14 +0100

    Feat: For deploy it page in GitHub Pages we need Hash Router

--- template/src/Router/Router.tsx
+++ template/src/Router/Router.tsx
@@ -1,7 +1,13 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import { routes } from '@src/Router/Routes'
 
-const router = createBrowserRouter(routes)
+/**
+ * We use Hash Router for GitHub Pages deployment, but you can use BrowserRouter
+ *
+ * const router = createBrowserRouter(routes)
+ */
+
+const router = createHashRouter(routes)
 
 export function Router() {
   return <RouterProvider router={router} />
