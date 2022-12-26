d08ec5dcaa3f0160f29a02744d9157a67b8a1fb0
Alejandro Pascual Montenegro <pascualmontenegro@gmail.com>
2022-12-26 10:44:53 +0100

    Feat: Add React Snap for publish a static web of this package

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,14 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import reportWebVitals from './reportWebVitals'
-import { Router } from '@src/Router/Router'
+import {Router} from '@src/Router/Router'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-  <React.StrictMode>
-    <Router />
-  </React.StrictMode>,
-)
+/**
+ * If you do not use react-snap for static page, you could change to the next line.
+ *
+ * const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+ * root.render(
+ *   <React.StrictMode>
+ *     <Router />
+ *   </React.StrictMode>
+ * )
+ *
+ */
+
+const rootElement = document.getElementById('root') as HTMLElement
+if (rootElement?.hasChildNodes()) {
+    ReactDOM.hydrateRoot(
+        rootElement,
+        <React.StrictMode>
+            <Router/>
+        </React.StrictMode>
+    )
+} else {
+    const root = ReactDOM.createRoot(rootElement)
+    root.render(
+        <React.StrictMode>
+            <Router/>
+        </React.StrictMode>
+    )
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
